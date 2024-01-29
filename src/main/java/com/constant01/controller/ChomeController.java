9664333a33b6428a74ded76914fd5c3291793509
GR <GR@GRN-304-05>
2024-01-29 10:02:16 +0900

    2024/01/29 10:02 kmc

--- src/main/java/com/constant01/controller/ChomeController.java
+++ src/main/java/com/constant01/controller/ChomeController.java
@@ -18,65 +18,51 @@ import com.constant01.service.DeliveryService;
 
 @Controller
 public class ChomeController {
-	/*
-	@Autowired
-	MemberService ms;
-	@Autowired
-	ProfService ps;
-	@Autowired
-	ReserveService rs;
-	*/
+
 	@Autowired
 	CustomerService cs;
 	@Autowired
 	DeliveryService ds;
 	
 	@RequestMapping(value = "company/homelog.do", method = RequestMethod.GET)
-	// 게시판 목록 리스트                                기본생성자 호출
 	public String homelog () {
 		return "company/homelog";
-		// list.jsp 실행 할 때 select 된 결과를 가져와라!!!!
+		
 	}
 	
-	//@RequestMapping(value = "/company/homelog.do", method = RequestMethod.POST)
+	
 	@PostMapping("/company/homelog.do")
 	public String login(DriverDTO driver, DeliveryDTO delivery, CustomerDTO customer,AdminDTO admin, HttpSession session, Model model) {
 		System.out.println("dddddd");
 		if(cs.login(customer) != null) {
-			// 로그인 해라 (session에 select값 저장)
 			System.out.println("회원 로그인");
 			session.setAttribute("login", cs.login(customer));
 			model.addAttribute("login");
-			// 구매 화면으로 이동
 			
 			model.addAttribute("dlist", ds.dlist(delivery));
-			return "/company/customertestpage";
+			return "release";
 			
 			//배달 기사 로그인
 		}else if(ds.login(driver) != null){
 			System.out.println("기사로그인");
-			session.setAttribute("login", ds.login(driver)); //로그인한 세션 값
+			session.setAttribute("login", ds.login(driver)); 
 			
-			model.addAttribute("list", ds.list(driver)); //list select한 값
-			
+			model.addAttribute("list", ds.list(driver));
 			return "/company/testpage";
 			
 		}else if(ds.login2(admin) != null){
 			System.out.println("관리자 로그인");
 			session.setAttribute("login2", ds.login2(admin));
 			model.addAttribute("login2");
-			model.addAttribute("dlist", ds.dlist(delivery)); //list select한 값
-			// 구매 화면으로 이동
+			model.addAttribute("dlist", ds.dlist(delivery)); 
 			
-			//model.addAttribute("dlist", ds.dlist(delivery));
 			
-			return "/company/shipper/ship_Master";
+			return "/company/adminPage.do";
 		
 		
-		}else {	// 그렇지 않으면 메인화면으로 이동...
+		}else {	
 			System.out.println("회원 로그인");
 			model.addAttribute("dlist", ds.dlist(delivery));
-			//model.addAttribute("list", ds.list(driver));
 			return "/company/homelog2";
 		}	
 	}
